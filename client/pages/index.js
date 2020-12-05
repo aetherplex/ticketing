1596f1046349e89714d1d16f932f4d309620b604
Dominic Hawton <dominic.hawton@mq.edu.au>
2020-12-05 13:34:48 +1100

    feat: add editorconfig file

---
+++ client/pages/index.js
@@ -0,0 +1,7 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { Heading } from '@chakra-ui/react';
+
+export default function Home() {
+	return <Heading>House</Heading>;
+}
